76ef9b33d51b5e8bdd3c2711c0a84a48ff8d341e
romi-sagi <romisagi24@gmail.com>
2025-10-28 14:19:36 +0200

    commit post tasks

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -3,7 +3,7 @@ import taskController from "../controllers/taskController";
 const router = express.Router();
 
 router.get('/tasks', taskController.getTasks);
-router.get('/:id', taskController.getTaskById)
-router.delete('/:id', taskController.deleteTaskById)
+router.get('/:id', taskController.getTaskById);
+router.delete('/:id', taskController.deleteTaskById);
 
 export default router;
