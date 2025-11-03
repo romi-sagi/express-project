f516f106c9581baea41328b85461cb0c616d7278
romi-sagi <romisagi24@gmail.com>
2025-11-03 13:18:06 +0200

    fix comments

--- src/validation.ts
+++ src/validation.ts
@@ -1,12 +1,12 @@
 import { Iteration, Task } from "./taskDto";
 
-const allowedRepeats = ["daily", "weekly", "monthly", "yearly", null];
+const allowedIterations = ["daily", "weekly", "monthly", "yearly", null];
 
-const isvalidIterationValue = (value: Iteration): boolean => {
-    return allowedRepeats.includes(value);
+const isValidIteration = (value: Iteration): boolean => {
+    return allowedIterations.includes(value);
 }
 
-const isvalidDateValue = (dueDate: Date): boolean => {
+const isValidDueDate = (dueDate: Date): boolean => {
     const newDate = new Date(dueDate);
 
     return !isNaN(newDate.getTime());
@@ -18,7 +18,7 @@ export const isValidTask = (task: Task): boolean => {
         typeof task.completed === 'boolean' &&
         typeof task.myDay === 'boolean' &&
         (task.description === undefined || typeof task.description === 'string') &&
-        (task.dueDate === undefined || isvalidDateValue(task.dueDate)) &&
-        (task.repeat === undefined || isvalidIterationValue(task.repeat))
+        (task.dueDate === undefined || isValidDueDate(task.dueDate)) &&
+        (task.repeat === undefined || isValidIteration(task.repeat))
     );
 }
