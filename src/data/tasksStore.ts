bb7855cfcebc39ee683e96dcb8bda361f24c193b
romi-sagi <romisagi24@gmail.com>
2025-11-12 16:51:00 +0200

    commit tasks store

--- src/data/tasksStore.ts
+++ src/data/tasksStore.ts
@@ -1,7 +1,3 @@
 import { Task } from "../taskDto";
 
-export const tasks: Task[] = [
-    { id: "njdnd", title: "fnjf", myDay: false, completed: false },
-    { id: "njnd", title: "jfjf", myDay: false, completed: true },
-    { id: "jdnd", title: "jfnf", myDay: true, completed: false },
-];
+export const tasks: Task[] = [];
