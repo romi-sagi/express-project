3af821f446d586e46c6cdcf8d4e67acc14a7bcdc
romi-sagi <romisagi24@gmail.com>
2025-11-13 13:33:42 +0200

    remove data

--- src/data/tasksStore.ts
+++ src/data/tasksStore.ts
@@ -1,7 +1,3 @@
 import { Task } from "../taskDto";
 
-export const tasks: Task[] = [
-    { id: "jiff", title: "jndjd", myDay: false, completed: true },
-    { id: "jf", title: "jndj", myDay: false, completed: true },
-    { id: "iff", title: "jnjd", myDay: false, completed: false }
-];
+export const tasks: Task[] = [];
